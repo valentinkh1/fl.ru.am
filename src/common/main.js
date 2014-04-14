9f0d017401a1683c0607b3ff8e65b2de69b78ce9
Andrew <fedyk72@gmail.com>
2014-04-14 16:53:44 +0300

    Add checker of new message from user

--- src/common/main.js
+++ src/common/main.js
@@ -1,15 +1,81 @@
-﻿function fill_form(templateId) {
-	var items = kango.storage.getItem('templates') || [];
-	var currentItem = {};
-	
-	if (templateId === undefined) return;
+﻿var extension = {
 
-	items.forEach(function(val, key) {
-		if (key === templateId) currentItem = val;
-	});
+  init: function() {
+    this.xhr = null;
+    this.delay = 300000;
+    this.timeoutId = null;
+    this.u_token_key = null;
+    this.updateUserToken();
+  },
 
-	kango.browser.tabs.getCurrent(function(tab) {
-		tab.dispatchMessage('fill_form', {template: currentItem});
-		console.log('[main.js]', 'fill_form', {template: currentItem});
-	});
-}
+  updateUserToken: function(callback){
+    var responseHandler = function(data){
+      if (data.status === 200 && data.response) {
+        this.u_token_key = this._parseUserToken(data.response);
+
+        if (this.u_token_key) {
+          this.fetchNotification();
+          return;
+        }
+      }
+
+      // Update token after delay
+      this.timeoutId = setTimeout(this.updateUserToken.bind(this), this.delay);
+    };
+
+    if (this.timeoutId) clearTimeout(this.timeoutId);
+
+    if (this.xhr) this.xhr.abort();
+
+    this.xhr = kango.xhr.send({
+      method: 'GET',
+      url: 'https://www.fl.ru/',
+      contentType: 'text'
+    }, responseHandler.bind(this));
+  },
+
+  fetchNotification: function() {
+
+    if (this.timeoutId) clearTimeout(this.timeoutId);
+
+    if (this.xhr) this.xhr.abort();
+
+    this.xhr = kango.xhr.send({ 
+      method: 'POST', 
+      url: 'https://www.fl.ru/notification.php',
+      params: {
+        op: 'msg',
+        u_token_key: this.u_token_key
+      },
+      contentType: 'json'
+    }, this.fetchNotificationHandler.bind(this));
+  },
+
+  fetchNotificationHandler: function(data) {
+    var info = data.response;
+
+    if (info && info.delay) {
+      this.delay = info.delay;
+    }
+
+    if (info && info.success && info.msg) {
+      kango.ui.browserButton.setBadgeValue(info.msg.count);
+      kango.ui.browserButton.setTooltipText(info.msg.tip);
+      kango.ui.browserButton.setBadgeBackgroundColor([114, 188, 78, 255]);
+    }
+
+    if (info && !info.success) {
+      kango.ui.browserButton.setBadgeValue(null);
+      kango.ui.browserButton.setTooltipText('FL.ru');
+    }
+
+    this.timeoutId = setTimeout(this.fetchNotification.bind(this), this.delay);
+  },
+
+  _parseUserToken: function(text){
+    var match = text && text.match(/_TOKEN_KEY[^']*'([^']+)';/);
+    return match && match[1];
+  }
+};
+
+extension.init();
